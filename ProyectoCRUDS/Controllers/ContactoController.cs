57b9fba9fbce87885990118bad15d3183fe745b4
Victor Avila <victor_15_avila@hotmail.es>
2023-07-18 02:50:31 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoCRUDS/Controllers/ContactoController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoCRUDS.Models;
+
+namespace ProyectoCRUDS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactoController : ControllerBase
+    {
+
+        private readonly DBPRUEBASContext _dbContext;
+        public ContactoController(DBPRUEBASContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            List<Contacto> lista = await _dbContext.Contactos.OrderByDescending(c => c.IdContacto).ToListAsync();
+
+            return StatusCode(StatusCodes.Status200OK, lista);
+        }
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] Contacto request)
+        {
+            await _dbContext.Contactos.AddAsync(request);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody] Contacto request)
+        {
+            _dbContext.Contactos.Update(request);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Contacto contacto =  _dbContext.Contactos.Find(id);
+            _dbContext.Contactos.Remove(contacto);
+            await _dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "ok");
+        }
+
+    }
+}
